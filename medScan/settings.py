1b90df90974b556e97f548bf2d864c3b386f8c22
Praful Sharma <praful.sharma_cs18@gla.ac.in>
2020-12-02 10:48:21 +0530

    added dataset for training

--- medScan/settings.py
+++ medScan/settings.py
@@ -76,13 +76,18 @@ WSGI_APPLICATION = 'medScan.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'medScan',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'qwerty',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'medScan',
-        'USER': 'postgres',
-        'PASSWORD': 'qwerty',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
